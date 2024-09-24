0dfc5aecc38d203274040b5887ec081992d97889
ayansheikh6600 <ayansheikh6600@gmail.com>
2024-09-24 22:28:40 +0500

    "Some changes"

--- src/layouts/Navbar/index.tsx
+++ src/layouts/Navbar/index.tsx
@@ -1,8 +1,10 @@
 "use client"
 // import { useRouter } from 'next/router';
 import { usePathname } from 'next/navigation'
+import { motion } from 'framer-motion';
 
 import Link from 'next/link';
+import { useState } from 'react';
 
 
 const navItems = [
@@ -16,6 +18,8 @@ const navItems = [
 
 const Navbar = () => {
   const pathname = usePathname()
+  const [selectedCategory, setSelectedCategory] = useState<string>('/');
+
 
 
 
@@ -23,29 +27,41 @@ const Navbar = () => {
 
 
   return (
-    <div className='flex justify-between items-center px-2 bg-gray-100 shadow-lg'>
+    <div className='flex justify-between items-center p-2 bg-gray-100 shadow-lg '>
       <div>
         LOGO
       </div>
-      <nav className="flex justify-center space-x-8 py-4 ">
+      <nav className="relative flex justify-center space-x-8 p-2 bg-black rounded-3xl">
         {navItems.map((item) => {
           const isActive = pathname === item.href;
 
           return (
-            <Link key={item.name} href={item.href}>
+            <Link key={item.name} href={item.href} onClick={() => setSelectedCategory(item?.href)}>
+              <div className='relative '>
+                
               <span
-                className={`relative px-4 py-2 font-medium text-gray-700 hover:text-indigo-500 
-                ${isActive ? 'outline-b outline-indigo-500' : ''}
-                hover:outline-b hover:outline-indigo-500`}
+                className={`relative z-10 px-4 py-6 font-medium hover:text-indigo-500 
+                ${selectedCategory === item?.href
+                      ? 'text-white'
+                      : 'text-gray-400'
+                  }
+                `}
               >
                 {item.name}
-                {/* Bottom outline for active or hover state */}
-                <span
-                  className={`absolute bottom-0 left-0 w-full h-1 transition-all duration-300 ease-in-out
-                ${isActive ? 'bg-indigo-500' : 'bg-transparent'}
-                group-hover:bg-indigo-500`}
-                />
+                
+                
               </span>
+              {selectedCategory === item?.href && (
+                  <motion.div
+                    layoutId="highlight"
+                    className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-[#1e064b] rounded-full"
+                    initial={false}
+                    transition={{ type: 'spring', stiffness: 500, damping: 30 }}
+                  />
+                )}
+
+              </div>
+
             </Link>
           );
         })}
